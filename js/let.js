996dfb58652fe25cc303e164d54a58b496f2e04a
SashaTkach <tkach8800@gmail.com>
2021-04-27 16:51:36 +0300

    Homework-2 done

---
+++ js/let.js
@@ -0,0 +1,15 @@
+let admin;
+let name;
+name = 'Джон';
+admin = name;
+alert (admin);
+
+
+let ourPlanet;
+let visitorSite;
+
+
+let number = 10;
+let string = '10';
+let sum = number + string;
+console.log(sum);
